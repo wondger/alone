200736c2964c39bae32ba158660681f603c90c8e
wondger <wondger@gmail.com>
2012-03-26 18:02:59 +0800

    Event

--- event/event.source.js
+++ event/event.source.js
@@ -29,43 +29,72 @@
         },
         isA:function(o){
             return ts.call(o) === '[object Array]';
-        }
-    },
-    E = {
-        on:function(ele,type,handle){
-            var type = /^(on)/.test(type) ? type.substr(2) : type;
-            if(ele.addEListener){
-                ele.addEListener(type,function(){handle.call(ele)},false);
-            }else if(ele.attachE){
-                ele.attachE('on'+type,function(){handle.call(ele)});
-            }else{
-                var _handle = ele['on'+type];
-                ele['on'+type] = function(){
-                    if(_handle) _handle.call(ele);
-                    handle.call(ele);
-                }
+        },
+        each:function(o,fn,scope){
+            if(!U.isO(o) || !U.isA(o) || !U.isF(fn)) return;
+
+            for(var k in o){
+                fn.call(scrop || null,o[k],k);
             }
         }
     },
-    std = [
-        'click',
-        'mouseenter',
-        'mouseover',
-        'mousedown',
-        'mouseleave',
-        'keyup',
-        'keydown',
-        'focus',
-        'blur'
-    ],
-    Event = function(receiver){
-        this.stdEvts = {};
-        this.cstEvnts = {};
-    };
-    Event.prototype = {
-        on:function(type,handle){
+    //standard Event
+    E = {
+        std:[
+            'click',
+            'mouseenter',
+            'mouseover',
+            'mousedown',
+            'mouseleave',
+            'keyup',
+            'keydown',
+            'focus',
+            'blur'
+        ],
+        on:function(el,type,handle){
+            if(!U.isS(type) || !U.isF(handle)) return this;
+            if(el.addEListener){
+                el.addEListener(type,function(){handle.call(el)},false);
+            }else if(el.attachE){
+                el.attachE('on'+type,function(){handle.call(el)});
+            }else{
+                var _handle = el['on'+type];
+                el['on'+type] = function(){
+                    if(_handle) _handle.call(el);
+                    handle.call(el);
+                }
+            }
+
+            return this;
+        },
+        off:function(el,type,handle){
+            return this;
+        },
+        delegate:function(to,el,type,handle){
+        },
+        undelegate:function(to,el,type,handle){
+        }
+    },
+    //guid must be private
+    guid = 0,
+    Event = {
+        evtId:'evt_' + new Date().getTime(),
+        __stdEvts__:{},
+        __cstEvts__:{},
+        on:function(el,type,handle){
+            if(!U.isS(type) || !U.isF(handle)) return false;
+            if(E.std.indexOf(type)>-1){
+                E.on(el,type,handle);
+            }else{
+                var evtId = el[this.evtId] = el[this.evtId] || ++guid;
+                this.__cstEvts__[evtId] = this.__cstEvts__[evtId] || {};
+                this.__cstEvts__[evtId][type] = this.__cstEvts__[evtId][type] || [];
+                this.__cstEvts__[evtId][type].push(handle);
+            }
         },
         fire:function(type){
         }
-    }
+    };
+
+    win.Alone = {'Event':Event};
 })(document,document.documentElement,window)
